ec0989aacd230b78f1ae44d43a7db455884d2b35
thanikasomasundaram-t <t.thillaikumar@cdw.com>
2023-03-27 12:38:44 +0530

    E1 completed.

---
+++ e1/index.js
@@ -0,0 +1,15 @@
+let fs = require('fs');
+let setRandomColors = [];
+
+let colors = JSON.parse(fs.readFileSync('./color_palette.json'));
+
+for(count = 0; count < 5; count ++) {
+    setRandomColors.push(colors[Math.floor(Math.random()*colors.length)]);
+}
+
+fs.writeFileSync('./random_colors.json', JSON.stringify(setRandomColors));
+
+let getRandomColors = JSON.parse(fs.readFileSync('./random_colors.json'));
+
+console.log("--------------------RANDOM FIVE COLORS----------------------")
+console.log(getRandomColors);
