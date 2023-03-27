b792dd10f9775b5f4348ea882857bf50a71b42ce
thanikasomasundaram-t <t.thillaikumar@cdw.com>
2023-03-27 14:21:18 +0530

    E2 completed.

--- e2/index.js
+++ e2/index.js
@@ -1,15 +1,26 @@
+let http = require('http');
 let fs = require('fs');
-let setRandomColors = [];
 
-let colors = JSON.parse(fs.readFileSync('./color_palette.json'));
 
-for(count = 0; count < 5; count ++) {
-    setRandomColors.push(colors[Math.floor(Math.random()*colors.length)]);
-}
+http.createServer((req, res, err) => {
+    let setRandomColors = [];
+    if (err) {
+        res.write(err);
+    }
+    let colors = JSON.parse(fs.readFileSync('./color_palette.json'));
 
-fs.writeFileSync('./random_colors.json', JSON.stringify(setRandomColors));
+    for (count = 0; count < 5; count++) {
+        setRandomColors.push(colors[Math.floor(Math.random() * colors.length)]);
+    }
 
-let getRandomColors = JSON.parse(fs.readFileSync('./random_colors.json'));
+    fs.writeFileSync('./random_colors.json', JSON.stringify(setRandomColors));
 
-console.log("--------------------RANDOM FIVE COLORS----------------------")
-console.log(getRandomColors);
+    let getRandomColors = JSON.parse(fs.readFileSync('./random_colors.json'));
+
+    console.log("--------------------RANDOM FIVE COLORS----------------------")
+    console.log(getRandomColors);
+
+    res.write(JSON.stringify(getRandomColors));
+    res.end();
+}).listen(4000);
+
