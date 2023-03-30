760f94ac70d7f72df77e5a121642ca18f4e226b3
thanikasomasundaram-t <t.thillaikumar@cdw.com>
2023-03-30 20:18:54 +0530

    refactored.

--- e1/index.js
+++ e1/index.js
@@ -1,28 +1,22 @@
-let fs = require('fs');
-let setRandomColors = [];
+let { readFileSync, writeFileSync }= require('fs');
+let setRandomColors = new Set();
 
 //read file from file color_palette
-let colors = JSON.parse(fs.readFileSync('./color_palette.json', 'UTF-8'));
-const randomColors = [];
+let colors = JSON.parse(readFileSync('./color_palette.json', 'UTF-8'));
+
 
 //loop till five random colors are pushed to array randomColors.
-var count = 5;
-while(count!=0) {
+while(setRandomColors.size < 5) {
     let randomNumber = Math.floor(Math.random()*colors.length);
-    if(randomColors.indexOf(randomNumber) == -1) {
-        console.log(randomNumber);
-        setRandomColors.push(colors[randomNumber]);
-        randomColors.push(randomNumber);
-        count-=1;
-    }
-    else {
-        console.log("copy", randomNumber);
-    }
+    setRandomColors.add(colors[randomNumber]);
 }
-//write the randomColors into file random_colors.
-fs.writeFileSync('./random_colors.json', JSON.stringify(setRandomColors));
 
-let getRandomColors = JSON.parse(fs.readFileSync('./random_colors.json'));
+
+
+writeFileSync('./random_colors.json', JSON.stringify(Array.from(setRandomColors)
+));
+
+let getRandomColors = JSON.parse(readFileSync('./random_colors.json'));
 
 console.log("--------------------RANDOM FIVE COLORS----------------------")
-console.log(setRandomColors);
+console.log(getRandomColors);
